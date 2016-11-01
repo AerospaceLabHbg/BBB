35c9bb90223973211989fc5e256d68c6465fe266
AerospaceLabHbg <kevin.hendriks@web.de>
2016-11-01 05:30:22 +0100

    Update bmp180.cpp

--- i2c/bmp180.cpp
+++ i2c/bmp180.cpp
@@ -92,8 +92,15 @@ int main(){
    //Setting mode to 00000000=0x00 for +/-2g 10-bit
    //Setting mode to 00001011=0x0B for +/-16g 13-bit
   // writeRegister(file, DATA_FORMAT, 0x00);
+	
+	this->registers = this->readRegisters(BUFFER_SIZE, 0x00);
+	if(*this->registers!=0x55){
+		perror("ADXL345: Failure Condition - Sensor ID not Verified");
+		return -1;
+	}
+	
    registers = sensor.readRegisters(BUFFER_SIZE,0x00);
-   cout << "The Device ID is: " << HEX(*(registers+DEVID9) << endl;
+   cout << "The Device ID is: " << (*(registers+DEVID9) << endl;
    //cout << "The POWER_CTL mode is: " << HEX(dataBuffer[POWER_CTL]) << endl;
    //cout << "The DATA_FORMAT is: " << HEX(dataBuffer[DATA_FORMAT]) << endl;
    //cout << dec << endl;   //reset back to decimal

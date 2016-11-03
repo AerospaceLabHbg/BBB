6c80fe766c3d68d816ef0db25478605bc08edd3c
AerospaceLabHbg <kevin.hendriks@web.de>
2016-11-03 18:16:17 +0100

    Update BMP180.cpp

--- BMP180.cpp
+++ BMP180.cpp
@@ -10,13 +10,37 @@ namespace exploringBB {
 
 
 #define	DEVID		0x00   //Device ID
-#define DATA_FORMAT	0x31   //Data format control
-#define OUTREG		0xF4   //Output control register
-
+#define SOFTRESET	0xE0   //Output control register
+	
+//Calibration registers 16bit MSB first
 #define AC11		0xAA
 #define AC12		0xAB
-
-
+#define AC21		0xAC
+#define AC22		0xAD
+#define AC31		0xAE
+#define AC32		0xAF
+#define AC41		0xB0
+#define AC42		0xB1
+#define AC51		0xB2
+#define AC52		0xB3
+#define AC61		0xB4
+#define AC62		0xB5
+#define B11		0xB6
+#define B12		0xB7
+#define B21		0xB8
+#define B22		0xB9
+#define MB1		0xBA
+#define MB2		0xBB
+#define MC1		0xBC
+#define MC2		0xBD
+#define MD1		0xBE
+#define MD2		0xBF
+//Output Manipulation
+#define CONREG		0xF4
+#define OUTTemp		0x2E
+#define OUTPres		0x34
+#define OSS		0
+	
 long BMP180::combineRegisters16(unsigned char msb, unsigned char lsb){
    //shift the MSB left by 8 bits and OR with LSB
    return ((short)msb<<8)|(short)lsb;
@@ -39,7 +63,7 @@ BMP180::BMP180(unsigned int I2CBus, unsigned int I2CAddress):
 	this->I2CAddress = I2CAddress;
 	this->I2CBus = I2CBus;
 	this->registers = NULL;
-	this->writeRegister(OUTREG, 0x2E);
+	this->writeRegister(SOFTRESET, 0xB6);
 }
 
 /*
@@ -48,14 +72,24 @@ BMP180::BMP180(unsigned int I2CBus, unsigned int I2CAddress):
  * and pass them to the combineRegisters() method to be processed.
  * @return 0 if the registers are successfully read and -1 if the device ID is incorrect.
  */
-	int BMP180::readSensorState(){
+int BMP180::readSensorState(){
 	this->registers = this->readRegisters(BUFFER_SIZE, 0x00);
 	if(*this->registers!=0x55){
 		perror("BMP180: Failure Condition - Sensor ID not Verified");
 		return -1;
 	}
 	this->ac1 = this->combineRegisters16(*(registers+AC11), *(registers+AC12));
+	this->ac2 = this->combineRegisters16(*(registers+AC21), *(registers+AC22));
+	this->ac3 = this->combineRegisters16(*(registers+AC31), *(registers+AC32));
+	this->ac4 = this->combineRegisters16(*(registers+AC41), *(registers+AC42));
+	this->ac5 = this->combineRegisters16(*(registers+AC51), *(registers+AC52));
+	this->ac6 = this->combineRegisters16(*(registers+AC61), *(registers+AC62));
 	return 0;
+}
+
+void BMP180::displayCalibrationData(){
+	cout << "AC1:"<< this->getAC1() << " AC2:" << this->getAC2() << "
+
 }
 	
 BMP180::~BMP180() {}

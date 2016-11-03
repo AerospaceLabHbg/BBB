ebf1179bfc7f80ba7236a8f9361396e4a4a6bb50
AerospaceLabHbg <kevin.hendriks@web.de>
2016-11-03 18:10:32 +0100

    Update BMP180.h

--- BMP180.h
+++ BMP180.h
@@ -13,7 +13,10 @@ class BMP180:protected I2CDevice{
 private:
 	unsigned int I2CBus, I2CAddress; //initialiserung für BMP180(I2CBus,I2CAddress)
 	unsigned char *registers;
-	short ac1, ac2 ,ac3;
+	short ac1, ac2, ac3, b1, b2, mb, mc, md;
+	unsigned short ac4, ac5, ac6;
+	long x1, x2, x3, b3, b5, b6;
+	unsigned long b4, b7;
 	long combineRegisters16(unsigned char msb, unsigned char lsb);
 	long combineRegisters24(unsigned char msb, unsigned char lsb, unsigned char xlsb);
 	long ut,up,pressure,temperature;

32c35dfde0d651d42f007865a06a5f7a8742bba0
AerospaceLabHbg <kevin.hendriks@web.de>
2016-11-06 10:45:27 +0100

    Update DS1307.cpp

--- DS1307.cpp
+++ DS1307.cpp
@@ -67,11 +67,27 @@ int DS1307::readSensorState(){
 }
 
 void DS1307::updateRTC(){
+	/*struct tm {
+   int tm_sec;          seconds,  range 0 to 59          
+   int tm_min;          minutes, range 0 to 59           
+   int tm_hour;         hours, range 0 to 23             
+   int tm_mday;         day of the month, range 1 to 31  
+   int tm_mon;          month, range 0 to 11             
+   int tm_year;         The number of years since 1900   
+   int tm_wday;        day of the week, range 0 to 6    
+   int tm_yday;         day in the year, range 0 to 365  
+   int tm_isdst;        daylight saving time             
+}; */
 	time_t t = time(NULL);
 	struct tm tm = *localtime(&t);
+	int tmhour, tmmon, tmyear, tmwday;
+	tmhour = tm.tm_hour+1;
+	tmmon = tm.tm_mon+1;
+	tmyear = tm.tm_year+1900;
 	cout << dec <<endl;
-	cout << "BBB Time->  "<< "seconds: "<< tm.tm_sec <<"   minutes: "<< tm.tm_min <<"   hours: "<< tm.tm_hour <<endl;
-	cout << "wday: "<< tm.tm_wday <<"   date: "<< tm.tm_mday <<"   month: "<< tm.tm_mon<<"   year: "<< tm.tm_year <<endl;
+	tmwday = tm.tm_wday+1;
+	cout << "BBB Time->  "<< "seconds: "<< tm.tm_sec <<"   minutes: "<< tm.tm_min <<"   hours: "<< tmhour <<endl;
+	cout << "wday: "<< tmwday <<"   date: "<< tmmday <<"   month: "<< tmmom<<"   year: "<< tmyear <<endl;
 
 
 }

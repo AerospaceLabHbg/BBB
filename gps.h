96ee2d056adda13c1e290c678a542104cbfedb9c
AerospaceLabHbg <kevin.hendriks@web.de>
2016-11-12 00:50:54 +0100

    Update gps.h

--- gps.h
+++ gps.h
@@ -25,25 +25,49 @@
 #ifndef I2C_H_
 #define I2C_H_
 
-#define BBB_I2C_0 "/dev/i2c-0"
-#define BBB_I2C_1 "/dev/i2c-1"
+ //This sets up variables for useful commands.
 
-/// An i2c Device has 0x100 registers
-#define BUFFER_SIZE 0x100
+                //This set is used to set the rate the GPS reports
+                #define UPDATE_10_sec  "$PMTK220,10000*2F\r\n" //Update Every 10 Seconds
+                #define UPDATE_5_sec  "$PMTK220,5000*1B\r\n"   //Update Every 5 Seconds  
+                #define UPDATE_1_sec  "$PMTK220,1000*1F\r\n"   //Update Every One Second
+                #define UPDATE_200_msec  "$PMTK220,200*2C\r\n" //Update Every 200 Milliseconds
+                //This set is used to set the rate the GPS takes measurements
+                #define MEAS_10_sec  "$PMTK300,10000,0,0,0,0*2C\r\n" //Measure every 10 seconds
+                #define MEAS_5_sec  "$PMTK300,5000,0,0,0,0*18\r\n"   //Measure every 5 seconds
+                #define MEAS_1_sec  "$PMTK300,1000,0,0,0,0*1C\r\n"   //Measure once a second
+                #define MEAS_200_msec "$PMTK300,200,0,0,0,0*2F\r\n"  //Meaure 5 times a second
+                //Set the Baud Rate of GPS
+                #define BAUD_57600 "$PMTK251,57600*2C\r\n"          //Set Baud Rate at 57600
+               #define BAUD_9600 "$PMTK251,9600*17\r\n"             //Set 9600 Baud Rate
+                //Commands for which NMEA Sentences are sent
+                #define GPRMC_ONLY "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n" //Send only the GPRMC Sentence
+                #define GPRMC_GPGGA"$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n"//Send GPRMC AND GPGGA Sentences
+                #define SEND_ALL "$PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n" //Send All Sentences
+                #define SEND_NOTHING"$PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n" //Send Nothing
 
-namespace exploringBB {
+/* baudrate settings are defined in <asm/termbits.h>, which is
+   included by <termios.h> */
+#define BAUDRATE B9600   // Change as needed, keep B
+#define BAUDRATEH B9600   // Change as needed, keep B
 
-/**
- * @class I2CDevice
- * @brief Generic I2C Device class that can be used to connect to any type of I2C device and read or write to its registers
- */
-class I2CDevice{
+/* change this definition for the correct port */
+#define MODEMDEVICE "/dev/ttyO4" //Beaglebone Black serial port
+
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+
+#define FALSE 0
+#define TRUE 1
+
+namespace GPS {
+
+class GPSDevice{
 private:
-	unsigned int bus;
-	unsigned int device;
-	int file;
+	int fd, c, res;
+   	struct termios oldtio, newtio;
+    	char buf[255];
 public:
-	I2CDevice(unsigned int bus, unsigned int device);
+	GPSDevice();
 	virtual int open();
 	virtual int write(unsigned char value);
 	virtual unsigned char readRegister(unsigned int registerAddress);
@@ -51,8 +75,11 @@ public:
 	virtual int writeRegister(unsigned int registerAddress, unsigned char value);
 	virtual void debugDumpRegisters(unsigned int number = 0xff);
 	virtual void close();
-	virtual ~I2CDevice();
+	virtual ~GPSDevice();
 };
+	
+               
+
 
 } /* namespace exploringBB */
 

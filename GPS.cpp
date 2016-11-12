a89ec5b78e56995a397be394e47dc73eb06ece1b
AerospaceLabHbg <kevin.hendriks@web.de>
2016-11-12 01:27:38 +0100

    Update GPS.cpp

--- GPS.cpp
+++ GPS.cpp
@@ -18,8 +18,7 @@ namespace GPS {
 
 GPSDevice::GPSDevice() {
                     
-    // Load the pin configuration
-    int ret = system("echo uart1 > /sys/devices/bone_capemgr.9/slots");
+
     /* Open modem device for reading and writing and not as controlling tty
        because we don't want to get killed if linenoise sends CTRL-C. */
     fd = open(MODEMDEVICE, O_RDWR | O_NOCTTY );

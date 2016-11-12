10d31d29f6876bab3d50439700add919f800515a
AerospaceLabHbg <kevin.hendriks@web.de>
2016-11-12 01:01:37 +0100

    Create testgps.cpp

---
+++ testgps.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream>
+#include "GPS.h"
+#include <unistd.h>
+#include <pthread.h>
+
+using namespace std;
+using namespace GPS;
+
+int main() {
+
+	gps GPSDevice();
+	
+	int i = 0;
+		while(i<5){
+gps.readnmea();
+	cout <<dec<<"i="<< i <<endl;
+	i++;	}
+	return 0;
+}

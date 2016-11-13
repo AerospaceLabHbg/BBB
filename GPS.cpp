9b61d51a32e698061e5ca8f3cb0dee14e8510853
AerospaceLabHbg <kevin.hendriks@web.de>
2016-11-13 18:36:31 +0100

    Update GPS.cpp

--- GPS.cpp
+++ GPS.cpp
@@ -49,9 +49,37 @@ GPSDevice::GPSDevice() {
     tcsetattr(fd,TCSANOW,&newtio);
     // NMEA command to ouput all sentences
     // Note that this code & format values in manual are hexadecimal
+	//------- Setting Baudrate to 57600
 	write(fd, BAUD_57600, sizeof(BAUD_57600));
         usleep(1000);
-	newtio.c_cflag = B57600 | CRTSCTS | CS8 | CLOCAL | CREAD;
+	close(fd);
+	  fd = open(MODEMDEVICE, O_RDWR | O_NOCTTY );
+    if (fd < 0) { perror(MODEMDEVICE); exit(-1); }
+
+    bzero(&newtio, sizeof(newtio)); /* clear struct for new port settings */
+
+    /* BAUDRATE: Set bps rate. You could also use cfsetispeed and cfsetospeed.
+       CRTSCTS : output hardware flow control (only used if the cable has
+                 all necessary lines. See sect. 7 of Serial-HOWTO)
+       CS8     : 8n1 (8bit,no parity,1 stopbit)
+       CLOCAL  : local connection, no modem contol
+       CREAD   : enable receiving characters */
+    newtio.c_cflag = BAUDRATEH | CRTSCTS | CS8 | CLOCAL | CREAD;
+
+    /* IGNPAR  : ignore bytes with parity errors
+       otherwise make device raw (no other input processing) */
+    newtio.c_iflag = IGNPAR;
+
+    /*  Raw output  */
+    newtio.c_oflag = 0;
+
+    /* ICANON  : enable canonical input
+       disable all echo functionality, and don't send signals to calling program */
+    newtio.c_lflag = ICANON;
+    /* now clean the modem line and activate the settings for the port */
+    tcflush(fd, TCIFLUSH);
+    tcsetattr(fd,TCSANOW,&newtio);
+	//---------
 	/*write(fd, UPDATE_200_msec, sizeof(UPDATE_200_msec));
         usleep(1000);
 	write(fd, MEAS_200_msec,sizeof(MEAS_200_msec));
